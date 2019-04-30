44f2348e859cd8dab592435413d7be4511d3568d
danitiemi <danitiemi.br@gmail.com>
2019-04-30 11:57:16 -0700

    First Heroku commit

---
+++ src/containers/Profile.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+
+// Contain the children routes
+class Profile extends Component {
+
+  render() {
+    return (
+      <div>
+        <h2>Profile!</h2>
+      </div>
+    )
+  }
+}
+
+export default Profile
--- src/containers/Template.js
+++ src/containers/Template.js
@@ -1,17 +1,27 @@
 import React, {Component} from 'react'
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider'
+import Button from '@material-ui/core/Button';
 
 // Contain the children routes
 class Template extends Component {
 
   render() {
     return (
-      <div>
-        <h1>PewBangPow</h1>
-        <main>
-          {this.props.children}
-        </main>
-      </div>
-     
+      <MuiThemeProvider>
+        <div>
+          <h1>PewBangPow</h1>
+          <Button 
+            variant="contained" 
+            color="primary" 
+            onClick={()=>{console.log('I work!')}}
+            >
+            Primary
+          </Button>
+          <main>
+            {this.props.children}
+          </main>
+        </div>
+      </MuiThemeProvider>
     )
   }
 }
